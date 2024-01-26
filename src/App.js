b7a11edddab2ab3aa4140cd858de9e455f782d39
DonghyeonKang <donghyeon3372@gmail.com>
2024-01-26 23:27:16 +0900

    Create booklist with dummyData

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+/* eslint-disable */
+import React, { useState, useEffect } from 'react';
+import Book from './components/Book';
+import dummyBookData from './constants/dummyBookData'
 import './App.css';
-
+import fetchBooks from './services/fetchBooks';
+// App 컴포넌트 선언
 function App() {
+  //const [books, setBooks] = useState([]);
+
+  // useEffect(() => {
+  //   // 컴포넌트가 마운트될 때 데이터를 가져옴
+  //   fetchBooks();
+  // }, []);
+
+  const [books, setBooks] = useState(dummyBookData); // TODO: 서버에서 받아오는 걸로 바꿔야함
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className='popularBooks'>
+        <h1>소설 인기 도서</h1>
+        <ul className='bookList'>
+            {books.map(book => (
+              <Book imgSrc={book.imagePath} title={book.title} author={book.author} />
+            ))}
+        </ul>
+      </section>
+      <section className='popularBooks'>
+        <h1>추리 스릴러</h1>
+        <ul className='bookList'>
+            {books.map(book => (
+              <Book imgSrc={book.imagePath} title={book.title} author={book.author} />
+            ))}
+        </ul>
+      </section>      
+    </main>
   );
 }
 
